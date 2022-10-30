bb0efff4b06a21e25a25ca7595f0006dafcf3f73
Mateus-IC1101 <mateus.dolglas6@gmail.com>
2022-10-30 13:11:29 -0300

    [Básico] - scrapting do clubes, api estrutura em construção para receber dados do scrapting

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>xxsx {{ data }}</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.getList();
+  },
+  data() {
+    return {
+      data: {},
+    };
+  },
+  methods: {
+    getList() {
+      this.axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
+      this.axios.get("http://127.0.0.1:5000/users").then((response) => {
+        console.log(response.data);
+        this.data = response.data;
+      });
+    },
+  },
+};
+</script>
